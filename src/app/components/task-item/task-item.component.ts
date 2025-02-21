d70291c9301cf59e9c844887851de6578bf7e0ea
Rodivan Bitencourt <53022344+RodivanPeterson@users.noreply.github.com>
2025-02-21 01:50:39 +0000

    cria o visual do componente task-item

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-task-item',
+  imports: [],
+  templateUrl: './task-item.component.html',
+  styleUrl: './task-item.component.css'
+})
+export class TaskItemComponent {
+  @Input() taskContent: string = '';
+  @Input() taskStatus: boolean = false;
+}
